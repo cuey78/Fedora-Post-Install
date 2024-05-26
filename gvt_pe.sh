1072c784d54fde2dbd81a8a3764a42b2aaf5d970
cuey78 <101441768+cuey78@users.noreply.github.com>
2024-05-26 18:07:17 +1000

    Add files via upload

--- gvt_pe.sh
+++ gvt_pe.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 modprobe kvmgt mdev vfio-iommu-type1
-echo "37d7d57d-de7b-47e0-ae05-d6e69b98d629" > "/sys/devices/pci0000:00/0000:00:02.0/mdev_supported_types/i915-GVTg_V5_2/create"
+
 
