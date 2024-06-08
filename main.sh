420ca41bda253eece564e91166ce2ca4bc388298
cuey78 <101441768+cuey78@users.noreply.github.com>
2024-06-08 20:39:44 +1000

    Add files via upload

--- main.sh
+++ main.sh
@@ -1,44 +1,75 @@
 #!/bin/bash
 
+#########################################################################################
+#                                                                                       #
+# Fedora Post-Install Script                                                            #
+# This script automates the configuration and setup of a Fedora system after            #
+# installation. It includes functions for installing packages, configuring system       #
+# settings, and setting up themes and environments.                                     #
+#                                                                                       #
+# Functions:                                                                            #
+#   banner - Displays a custom banner with script information.                          #
+#   show_progress - Shows the progress of file imports using a gauge dialog.            #
+#   source_function_scripts - Sources additional script files containing utility        #
+#                             functions.                                                #
+#   check_and_install_package - Checks for the presence of a package and installs it    #
+#                               if not present.                                         #
+#   notify - Sends a desktop notification with a specified message.                     #
+#   log_action - Logs actions to a file with a timestamp.                               #
+#   check_functions - Checks if required functions are defined in the environment.      #
+#   menu - Displays a menu for user interaction to select various setup options.        #
+#   FIX_GRUB - Modifies the GRUB bootloader configuration.                              #
+#   update_grub_config - Updates the GRUB configuration files for BIOS and UEFI.        #
+#                                                                                       #
+# Usage:                                                                                #
+#   Run the script with root privileges to ensure proper functionality.                 #
+#   Select options from the menu to perform specific setup tasks.                       #
+#                                                                                       #
+# Author: cuey                                                                          #
+#                                                                                       #
+#########################################################################################
+
 # Shows Banner on load
 banner(){
     color1="\033[0;31m" # Red color
-    color2="\033[0;34m"
+    color2="\033[0;34m" # Blue color
     # Reset color
     reset_color="\033[0m"
 
     clear
-    printf "${color1}
-    ######## ######## ########   #######  ########     ###
-    ##       ##       ##     ## ##     ## ##     ##   ## ##
-    ##       ##       ##     ## ##     ## ##     ##  ##   ##
-    ######   ######   ##     ## ##     ## ########  ##     ##
-    ##       ##       ##     ## ##     ## ##   ##   #########
-    ##       ##       ##     ## ##     ## ##    ##  ##     ##
-    ##       ######## ########   #######  ##     ## ##     ##
+    # Get terminal width
+    term_width=$(tput cols)
+    # Calculate padding to center the banner
+    padding=$(printf '%*s' $(( (term_width - 58) / 2 )) '')
 
-             ########   #######   ######  ########
-             ##     ## ##     ## ##    ##    ##
-             ##     ## ##     ## ##          ##
-             ########  ##     ##  ######     ##
-             ##        ##     ##       ##    ##
-             ##        ##     ## ##    ##    ##
-             ##         #######   ######     ##
-
-   #### ##    ##  ######  ########    ###    ##       ##
-    ##  ###   ## ##    ##    ##      ## ##   ##       ##
-    ##  ####  ## ##          ##     ##   ##  ##       ##
-    ##  ## ## ##  ######     ##    ##     ## ##       ##
-    ##  ##  ####       ##    ##    ######### ##       ##
-    ##  ##   ### ##    ##    ##    ##     ## ##       ##
-   #### ##    ##  ######     ##    ##     ## ######## ########
-        
-    ${color2}
-        https://github.com/cuey78/Fedora-Post-Install
-      -------------------------------------------------
-${reset_color}
-"
+    echo -e "${color1}${padding}######## ######## ########   #######  ########     ###"
+    echo -e "${color1}${padding}##       ##       ##     ## ##     ## ##     ##   ## ##"
+    echo -e "${color1}${padding}##       ##       ##     ## ##     ## ##     ##  ##   ##"
+    echo -e "${color1}${padding}######   ######   ##     ## ##     ## ########  ##     ##"
+    echo -e "${color1}${padding}##       ##       ##     ## ##     ## ##   ##   #########"
+    echo -e "${color1}${padding}##       ##       ##     ## ##     ## ##    ##  ##     ##"
+    echo -e "${color1}${padding}##       ######## ########   #######  ##     ## ##     ##"
+    echo -e "${color1}${padding}"
+    echo -e "${color1}${padding}########   #######   ######  ########"
+    echo -e "${color1}${padding}##     ## ##     ## ##    ##    ##"
+    echo -e "${color1}${padding}##     ## ##     ## ##          ##"
+    echo -e "${color1}${padding}########  ##     ##  ######     ##"
+    echo -e "${color1}${padding}##        ##     ##       ##    ##"
+    echo -e "${color1}${padding}##        ##     ## ##    ##    ##"
+    echo -e "${color1}${padding}##         #######   ######     ##"
+    echo -e "${color1}${padding}"
+    echo -e "${color1}${padding}#### ##    ##  ######  ########    ###    ##       ##"
+    echo -e "${color1}${padding} ##  ###   ## ##    ##    ##      ## ##   ##       ##"
+    echo -e "${color1}${padding} ##  ####  ## ##          ##     ##   ##  ##       ##"
+    echo -e "${color1}${padding} ##  ## ## ##  ######     ##    ##     ## ##       ##"
+    echo -e "${color1}${padding} ##  ##  ####       ##    ##    ######### ##       ##"
+    echo -e "${color1}${padding} ##  ##   ### ##    ##    ##    ##     ## ##       ##"
+    echo -e "${color1}${padding}#### ##    ##  ######     ##    ##     ## ######## ########"
+    echo -e "${color1}${padding}"
+    echo -e "${color2}${padding}    https://github.com/cuey78/Fedora-Post-Install"
+    echo -e "${reset_color}"
 }
+
 # Ensure the script runs with elevated privileges
 if [ $EUID -ne 0 ]; then
     banner
@@ -46,43 +77,37 @@ if [ $EUID -ne 0 ]; then
     exit 1
 fi
 
-#shows progress of importing scripts
+# Function to show progress of importing scripts
 show_progress() {
-    total_files=1  # Assuming you want to import 5 files
-    for ((i = 0; i <= total_files; i++)); do
-        echo "$((i * 0))"
-        echo "$((i * 100))"
-        sleep 1
-    done | dialog --title "Importing function scripts..." --gauge "Importing script $file" 10 70 0
+    local total_files=$1
+    local current_file_index=$2
+    local percentage=$((current_file_index * 100 / total_files))
+
+    echo $percentage | dialog --gauge "Importing function scripts..." 10 70
+    sleep .1
 }
 
+# Function to source function scripts from the src directory
 source_function_scripts() {
-    for i in {1..5}; do
-        file="./src/data$i.db"
-        if [ -e "$file" ]; then
-            show_progress "$file"
-            source "$file"
+    local files_to_source=(./src/data*.db)
+    local total_files=${#files_to_source[@]}
+    local current_file_index=0
 
+    for file in "${files_to_source[@]}"; do
+        if [ -e "$file" ]; then
+            source "$file"
             if [ $? -ne 0 ]; then
                 echo "Failed to source $file"
                 exit 1
             fi
+            ((current_file_index++))
+            show_progress "$total_files" "$current_file_index"
         else
             echo "$file does not exist in the current directory."
             exit 1
         fi
     done
 }
-
-# Dialog dimensions
-HEIGHT=0
-WIDTH=0
-CHOICE_HEIGHT=11
-
-# Titles and messages
-TITLE="Fedora Post Install Script"
-MENU="Please Choose one of the following options:"
-
 
 # Check for necessary packages and install if missing
 check_and_install_package() {
@@ -129,9 +154,18 @@ check_functions() {
     done
 }
 
-menu(){
-    # Options for the menu
-    OPTIONS=(
+# Constants for dialog dimensions
+declare -r HEIGHT=0
+declare -r WIDTH=0
+declare -r CHOICE_HEIGHT=11
+
+# Constants for titles and messages
+declare -r TITLE="Fedora Post Install Script"
+declare -r MENU="Please Choose one of the following options:"
+
+menu() {
+    # Menu options array
+    local options=(
         1 "Fix and Clean DNF"
         2 "Check for Firmware update"
         3 "Install RPM Fusion"
@@ -144,18 +178,19 @@ menu(){
         10 "Extras"
         Q "Quit"
     )
-    # Main loop
+
+    # Infinite loop for menu display and interaction
     while true; do
-        CHOICE=$(dialog --clear \
-                    --title "$TITLE" \
-                    --nocancel \
-                    --menu "$MENU" \
-                    $HEIGHT $WIDTH $CHOICE_HEIGHT \
-                    "${OPTIONS[@]}" \
-                    2>&1 >/dev/tty)
+        local choice=$(dialog --clear \
+                              --title "$TITLE" \
+                              --nocancel \
+                              --menu "$MENU" \
+                              $HEIGHT $WIDTH $CHOICE_HEIGHT \
+                              "${options[@]}" \
+                              2>&1 >/dev/tty)
 
         clear
-        case $CHOICE in
+        case $choice in
             1) fix_and_clean_dnf ;;
             2) check_firmware_update ;;
             3) install_rpm_fusion ;;
@@ -167,26 +202,26 @@ menu(){
             9) nfs_setup ;;
             10) fedora_theme_fix ;;
             Q) log_action "User chose to quit the script."; exit 0 ;;
-            *) log_action "Invalid option selected: $CHOICE";;
+            *) log_action "Invalid option selected: $choice";;
         esac
     done
 }
 
-# Start of the script
+# Initialization of the script
 
-#Install dependencies
+# Install required dependencies
 check_and_install_package dialog
 check_and_install_package xdotool
 
-#show Banner
+# Display the initial banner
 banner
-#sleep for 2
+# Pause execution for 2 seconds to allow banner visibility
 sleep 2
-#show progress and imports scripts
+# Load additional script functions
 source_function_scripts
-#checks Imported functions
+# Verify that all necessary functions are correctly imported
 check_functions
-#Displays Main Menu
+# Display the main menu to the user
 menu
-#show banner on exit
+# Show the closing banner upon script exit
 banner
