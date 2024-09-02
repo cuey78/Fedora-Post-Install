9c37dd2d033265aee7a97b08f4955c4cf5b63bf7
cuey78 <101441768+cuey78@users.noreply.github.com>
2024-09-02 11:33:46 +1000

    Add files via upload

--- main.sh
+++ main.sh
@@ -38,6 +38,50 @@
 #  Reset color                                                                          #
 #  RESET="\033[0m"                                                                      #
 #########################################################################################
+
+Clone_Repo() {
+  local repo_url="https://github.com/cuey78/fedora-post-install"
+  local clone_dir="Fedora-post-install"
+
+  # Check if git is installed
+  if ! command -v git &> /dev/null; then
+    echo "Git not found. Installing git..."
+    sudo dnf install -y git  # Assuming Fedora uses dnf package manager
+  fi
+
+  if [ ! -d "$clone_dir" ]; then
+    git clone $repo_url $clone_dir
+  else
+    cd $clone_dir
+    git pull
+    cd -
+  fi
+}
+
+Run_Script() {
+  local script_path="$1/main.sh"
+
+  if [ -f "$script_path" ]; then
+    sudo bash $script_path
+  else
+    echo "Script $script_path does not exist."
+  fi
+}
+
+main() {
+  Clone_Repo
+
+  cd Fedora-post-install
+
+  chmod +x main.sh
+
+  ./main.sh
+}
+
+if [ "${0##*/}" == "/usr/bin/env" ]; then
+  exec ${0:-/dev/null}
+fi
+
 # Shows Banner on load
 banner(){
     local color1="\033[0;37m" # Red color
@@ -241,6 +285,16 @@ check_and_install_package xdotool
 
 # Display the initial banner
 banner
+
+# Check if running for the first time
+if [ ! -d "Fedora-post-install" ] && [ ! -f "main.sh" ]; then
+    # If the directory and main script don't exist, run main function
+    main
+else
+    # If files/folders exist, assume it's not the first run
+    echo "Fedora post-install files already exist. Skipping initial setup."
+fi
+
 # Pause execution for .5 seconds to allow banner visibility
 sleep 1
 # Load additional script functions
